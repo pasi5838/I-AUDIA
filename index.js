900c8042a1915f3d77c22e1a24d3959d0dc95229
pasi5838 <rikitidore39@gmail.com>
2025-06-11 19:22:33 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const quizData = [
+  { name: "Kucing", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/81_INF_DIV_SSI.jpg/1200px-81_INF_DIV_SSI.jpg", options: ["Kucing", "Anjing", "Gajah", "Ayam"] },
+  { name: "Anjing", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Shiba_Inu_dog.jpg/640px-Shiba_Inu_dog.jpg", options: ["Kucing", "Anjing", "Gajah", "Ayam"] }
+  // Tambahkan hingga 100 soal di versi lengkap
+];
